c45ed119eb95c89849e2dfd37b84491735369f4f
NarineAvetisyan <nare2005@mail.ru>
2022-04-26 14:28:01 +0400

    add css

---
+++ index.js
@@ -0,0 +1,23 @@
+// let list = document.querySelector(".list")
+// let but = document.querySelector(".btn_delete")
+// list.onmouseenter = function(){
+//     but.style.display = "inline"
+    
+// }
+// list.onmouseleave = function () {
+//     but.style.display = "none"
+// }
+
+
+// let list = document.querySelectorAll(".list")
+
+// list.forEach(function(val){
+//     list.onmouseenter = function(){
+//         btn.style.display = "inline" }})
+//  function mouseOver(el){
+//      list.forEach(element => {
+//          if(element.id ===el.id){
+//              //show element
+//          }
+//      });
+//  }
